1aa445774e488d08e6d8f9b9285de13ab03f27f9
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-01-21 13:57:27 +0100

    Course day 37. Sending email on camera movement with recorded image in it.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import glob
+import os
 import cv2
 import send_email as se
+from threading import Thread
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 
 video = cv2.VideoCapture(0)
 
 first_frame = None
-
 status_list = []
+count = 0
 
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -24,18 +33,26 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            num = int(len(all_images)/2)
+            image_export = all_images[num]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        se.send_email()
+        email_thread = Thread(target=se.send_email, args=(image_export, ))
+        email_thread.daemon = True
+
+        email_thread.start()
 
     cv2.imshow("Capture", frame)
 
@@ -45,3 +62,5 @@ while True:
         break
 
 video.release()
+
+clean_folder()
--- send_email.py
+++ send_email.py
@@ -1,2 +1,29 @@
-def send_email():
-    print("tick")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+import os
+
+
+def send_email(image):
+    password = os.environ["SMTP_PASSWORD"]
+    email = "officeguyyt@gmail.com"
+
+    email_msg = EmailMessage()
+    email_msg["Subject"] = "Camera detected movement!"
+    email_msg.set_content("Here is an image.")
+
+    with open(image, "rb") as file:
+        content = file.read()
+
+    email_msg.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    mail = smtplib.SMTP("smtp.gmail.com", 587) # why not 465?
+    mail.ehlo()
+    mail.starttls()
+    mail.login(email, password)
+    mail.sendmail(email, email, email_msg.as_string())
+    mail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image="Metro.jpg")
